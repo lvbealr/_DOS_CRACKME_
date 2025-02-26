313427a5f798121806d02ed07eb4b0b5d9658083
lvbealr <lvbealr@mail.ru>
2025-02-26 15:03:41 +0300

    delete useless string
    del tests

--- data.asm
+++ data.asm
@@ -2,4 +2,3 @@ HELLO_MESSAGE	db 	"Hello! I'm CrackMe!",	0dh, 0ah, '$'
 ENTER_MESSAGE	db 	"Enter a password: ",		  '$'
 ACCESS_GRANTED	db 0ah, "Access GRANTED!", 	0dh, 0ah, '$'
 ACCESS_DENIED	db 0ah, "Access DENIED!",  	0dh, 0ah, '$'
-PATH_TO_FILE	db 	"S:\_DOS_C~1\str.txt",	00h
